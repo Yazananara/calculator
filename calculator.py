5f954199dbb1c0d0db0c54a3a99653d0b63e8f71
Yazananara <107859706+Yazananara@users.noreply.github.com>
2022-07-04 16:26:38 +0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print("simple calculator")
+a=10
+b=30
+sum=a+b
+defrence= a-b
+product= a*b
+quotient= a/b
+remainder= a%b
+print("the sum is",sum)
+print("the defrence is",defrence)
+print("the product is",product)
+print("the quotient is",quotient)
+print("the remainder is",remainder)
